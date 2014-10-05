c2c6a7e68aaeba6cee13c6dbfe015d58bac3cf49
A.J. Norton <ajn123@vt.edu>
2014-10-05 13:07:48 -0400

    update background image across all sites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,24 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :show, :update]
+
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
 
   def update
-    @user = User.find(params[:id])
     @user.update_attributes(user_params)
     render 'show'
   end
 
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :about)
